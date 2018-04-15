c82fd3f3cbfaafeaea5c98b1b2d0f8d955227a13
Justin Michaud <justin@justinmichaud.com>
2018-04-15 16:15:34 -0700

    add support for gesturing words with accents

--- app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardWithGestureTyping.java
+++ app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardWithGestureTyping.java
@@ -2,7 +2,6 @@ package com.anysoftkeyboard.ime;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.inputmethod.InputConnection;
 
 import com.anysoftkeyboard.api.KeyCodes;
