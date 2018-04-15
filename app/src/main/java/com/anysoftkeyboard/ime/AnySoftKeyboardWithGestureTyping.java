15f39ee0d1ef95a22bdb2bdfa8232aa67ee9bfc9
Justin Michaud <justin@justinmichaud.com>
2018-04-15 13:59:16 -0700

    allow keyboard to generate gesture paths when using multiple languages

--- app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardWithGestureTyping.java
+++ app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardWithGestureTyping.java
@@ -2,6 +2,7 @@ package com.anysoftkeyboard.ime;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.inputmethod.InputConnection;
 
 import com.anysoftkeyboard.api.KeyCodes;
